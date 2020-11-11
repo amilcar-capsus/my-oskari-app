17b27e08fae6e0295b0be4237371151210fe1276
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2020-11-11 13:39:09 -0600

    Test

--- applications/geoportal/index.js
+++ applications/geoportal/index.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function() {
     Oskari.app.loadAppSetup(ajaxUrl + 'action_route=GetAppSetup', window.controlParams, function() {
         jQuery('#mapdiv').append('Unable to start');
+    }, function() {
+        Oskari.app.playBundle({ bundlename: 'download-basket' });
     });
 });
